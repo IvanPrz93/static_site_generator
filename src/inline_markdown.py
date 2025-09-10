c13c5d539f6c73d339ec9888881cfebf53b12114
IvanPrz93 <ivan.prz99693@gmail.com>
2025-09-09 23:22:45 -0600

    Inline: Added Markdown to TextNodes functions and splitters

---
+++ src/inline_markdown.py
@@ -0,0 +1,85 @@
+import re
+
+from textnode import TextNode, TextType
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    bold_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    italic_nodes = split_nodes_delimiter(bold_nodes, "_", TextType.ITALIC)
+    code_nodes = split_nodes_delimiter(italic_nodes, "`", TextType.CODE)
+    image_nodes = split_nodes_image(code_nodes)
+    return split_nodes_link(image_nodes)
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            split_text = node.text.split(delimiter)
+            if len(split_text) % 2 == 0:
+                raise Exception(f"Invalid syntax: no matching closing {delimiter} was found")
+            for i in range(len(split_text)):
+                if split_text[i] != "":
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(split_text[i], TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(split_text[i], text_type))
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            images = extract_markdown_images(text)
+            if not images:
+                new_nodes.append(node)
+            else:
+                for image in images:
+                    image_alt = image[0]
+                    image_link = image[1]
+                    split_text = text.split(f"![{image_alt}]({image_link})", 1)
+                    if split_text[0] != "":
+                        new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                    new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                    text = split_text[1]
+                if text != "":
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            links = extract_markdown_links(text)
+            if not links:
+                new_nodes.append(node)
+            else:
+                for link in links:
+                    link_text = link[0]
+                    link_url = link[1]
+                    split_text = text.split(f"[{link_text}]({link_url})", 1)
+                    if split_text[0] != "":
+                        new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                    new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+                    text = split_text[1]
+                if text != "":
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
